0413d8fc2be73bd0f70122368144fb703498f4f2
tiagomonteiro0715 <tiago.monteiro0715@gmail.com>
2022-11-19 18:03:32 +0000

    showtray done

--- final.c
+++ final.c
@@ -21,7 +21,11 @@ Não, isso é na função de apresentação. no ficheiro texto está linha por l
 
 como fazer com que o menu apareça uma só vez??
 
-apresentada numa matriz 4x4, com o ID do lote e o seu tipo (ver Figura 6). - onde é que o tipo fica?? é suposto contagenar o IR com o tipo? -- ID_Tipo
+apresentada numa matriz 4x4, com o ID do lote e o seu tipo (ver Figura 6). - onde é que o tipo fica?? é suposto contagenar o IR com o tipo? -- ID_Tipo 
+
+
+
+showTray - está bom assim ou coloco uma matriz para ficar tudo alinhado?
 */
 
 #include <stdio.h>
@@ -56,41 +60,15 @@ typedef struct prateleira {
 }
 PRATELEIRA;
 
-LOTE exemplo_Um_Binario = {
-  1,
-  "Sintra",
-  "2022-07-16",
-  50,
-  2
-};
-LOTE exemplo_Dois_Binario = {
-  2,
-  "Lisboa",
-  "2022-08-05",
-  50,
-  2
-};
-LOTE exemplo_Tres_Binario = {
-  3,
-  "Lisboa",
-  "2022-05-27",
-  50,
-  2
-};
-LOTE exemplo_Quatro_Binario = {
-  4,
-  "Porto",
-  "2022-03-12",
-  50,
-  2
-};
-LOTE exemplo_Cinco_Binario = {
-  5,
-  "Sintra",
-  "2022-11-07",
-  50,
-  2
-};
+void le_lote(LOTE * ptr) {
+  printf("\n===Conteudos lote===\n id: %d\n destiny:%s\n date:%s\n quantity:%d\n type:%d\n n",
+    ( * ptr).id,
+    ( * ptr).destiny,
+    ( * ptr).date,
+    ( * ptr).quantity,
+    ( * ptr).type
+  );
+}
 
 //-----------------------------------------------------------------------------------------------------------
 
@@ -98,37 +76,65 @@ int showTray() {
   char inputTray[100];
   int testingVar;
   FILE * fp;
-
+  char str[60];
+  int i = 1;
   getchar(); // Para apanhar o [enter]
 
   printf("Filename: ");
   scanf("%s", inputTray);
-
+  printf("\n");
   fp = fopen(inputTray, "r");
   if (fp == NULL) {
     printf("Error opening text file\n");
     exit(1);
   }
 
-  while ((EOF)) {
-    fscanf(fp, "%d ,%[^  ]", & testingVar, inputTray);
-
-    printf("\n\nid: %d name: %s\n\n", testingVar, inputTray);
-
+  while (fgets(str, sizeof(str), fp)) {
+    fscanf(fp, "%d", & testingVar);
+    printf("%d ", testingVar);
+    if (!(i % 4)) {
+      printf("\n");
+    }
+    i = i + 1;
   }
   //https://www.youtube.com/watch?v=TzNhPOwjlb0
   //depois deste video usar só read para ler o ficheiro .dat
   //ver trabalho de ano passado na drive do NEEC como leram ficheiros .dat e como está no livro
 
-  /*FIQUEI AQUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII*/
   fclose(fp);
   return 0;
 
 }
+//-----------------------------------------------------------------------------------------------------------
+
+int showCompleteBatch() {
+  FILE * fp = fopen("warehouse.dat", "rb");
+
+  if (fp == NULL) {
+    printf("Error opening text file\n");
+    exit(1);
+  }
+
+  /*
+  fread binary file c
+
+  como ver estrutura de ficheiro binário??
+
+  showtray - fazer array para colocar lá os valores. fica melhor assim. como é uma função, depois de ussada a memoria é apagada.
+
+  ver trabalho de ano passado emeplo para ver como eles virem ficheiro .dat to paulo pinto
+
+  https://stackoverflow.com/questions/35820879/read-a-dat-file-in-c
+
+  ler livro de PM
+  */
+
+  return 0;
+}
 
 void showMenu() {
 
-  printf("1 - Show tray\n");
+  printf("\n\n1 - Show tray\n");
   printf("2 - Show batch info\n");
   printf("3 - List batches\n");
   printf("4 - Show wharehouse occupancy\n");
@@ -163,6 +169,8 @@ void choices() {
       showMenu();
       break;
     case '2':
+      showCompleteBatch();
+      showMenu();
       break;
     case '3':
       break;
