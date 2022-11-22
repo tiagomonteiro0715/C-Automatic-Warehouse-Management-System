bb714945977db937fe487ebcb8071e20c6c3f52b
Tiago Monteiro <tiago.monteiro0715@gmail.com>
2022-11-22 10:20:22 +0000

    Shows all batches

--- final.c
+++ final.c
@@ -16,22 +16,25 @@
 #define LATERAL_SIZE 9
 
 typedef struct lote {
-  int id;
+  unsigned int id;
   char destiny[MAX_DESTINY_STR];
   char date[MAX_DATE_STR];
-  int quantity;
-  int type;
+  unsigned int quantity;
+  unsigned int type;
 }
 LOTE;
 
+
+/*
 typedef struct prateleira {
   size_t LOTE;
   char Position[LATERAL_SIZE][LATERAL_SIZE][NUM_PRATELEIRAS];
 }
 PRATELEIRA;
+*/
 
 void le_lote(LOTE * ptr) {
-  printf("\n===Conteudos lote===\n id: %d\n destiny:%s\n date:%s\n quantity:%d\n type:%d\n n",
+  printf("\n\n===Conteudos lote===\n id: %u\n destiny:%s\n date:%s\n quantity:%u\n type:%u\n\n",
     ( * ptr).id,
     ( * ptr).destiny,
     ( * ptr).date,
@@ -40,6 +43,7 @@ void le_lote(LOTE * ptr) {
   );
 }
 
+LOTE exemploLote;
 //-----------------------------------------------------------------------------------------------------------
 
 int showTray(bool override) {
@@ -49,19 +53,17 @@ int showTray(bool override) {
   char str[60];
   int i = 1;
 
-  if (override== FALSE){
-  getchar(); // Para apanhar o [enter]
-  printf("Filename: ");
-  scanf("%s", inputTray);
-  printf("\n");
-  fp = fopen(inputTray, "r");
-  }else{
+  if (override == FALSE) {
+    getchar(); // Para apanhar o [enter]
+    printf("Filename: ");
+    scanf("%s", inputTray);
+    printf("\n");
+    fp = fopen(inputTray, "r");
+  } else {
     fp = fopen("tray.txt", "r");
   }
 
-
-
-  if (fp == NULL) {//d´a NULL porque n~ao est´a a apontar para nada
+  if (fp == NULL) { //d´a NULL porque n~ao est´a a apontar para nada
     printf("Error opening text file\n");
     exit(1);
   }
@@ -74,18 +76,16 @@ int showTray(bool override) {
     }
     i = i + 1;
   }
-  
 
-//tentativa de ler ficheiro warehouse.dat
-/*
-  char buffer[10];
-   while (feof(fp) != EOF) // to read file
-    {
-      fread(buffer, sizeof(char), 1, fp);
-      printf("\n%s", buffer);
-   }
-*/
-
+  //tentativa de ler ficheiro warehouse.dat
+  /*
+    char buffer[10];
+     while (feof(fp) != EOF) // to read file
+      {
+        fread(buffer, sizeof(char), 1, fp);
+        printf("\n%s", buffer);
+     }
+  */
 
   fclose(fp);
   return 0;
@@ -100,30 +100,45 @@ int showCompleteBatch() {
     printf("Error opening text file\n");
     exit(1);
   }
-  
+
+  size_t varTest;
+  int count;
+  LOTE * buff = malloc(sizeof(LOTE));
+  while ((varTest = fread(buff, sizeof(LOTE), 1, fp)) > 0) {
+    le_lote(buff);
+  }
+  free(buff);
 
   /*
   fread binary file c
  tipo 1 - C - cartoes
  tipo 2 - L - livretes
- 
+
  por isto bem na funç~ao showtray.txt
  
  conctagenar ambos com _ e funç~ao de convers~ao de numero para letra. usar strcpy
   */
-  
-  
-  
+
   /*
-  usar 5 freads. Um para cada tipo de informaçao separado por um espaço
-  4 freads:
-  1 para id e tipo
-  1 para destino
-  1 para quantidade
-  1 para data de expediçao
+  fazer fwrite para um ficheiro texto para usar fscanf e para ver se está a ler bem o ficheiro
+  https://www.tutorialspoint.com/c_standard_library/c_function_fwrite.htm
+
+  senão tenho que mandar email ao professor
+
+  posso fazer isso??
+
+  pela linha de comandos está a haver problemas
+
+  correr contigo na maquina virtual
   */
 
-
+  /*links que me ajudaram
+  https://stackoverflow.com/questions/21721808/how-to-fread-structs
+  https://stackoverflow.com/questions/37779302/fread-into-struct-reads-data-incorrectly
+  https://stackoverflow.com/questions/16075233/reading-and-processing-wav-file-data-in-c-c
+  https://stackoverflow.com/questions/61834780/reading-binary-file-and-storing-it-to-struct-in-c?noredirect=1&lq=1
+  https://stackoverflow.com/questions/21721808/how-to-fread-structs
+  */
   return 0;
 }
 
@@ -164,10 +179,10 @@ void choices() {
       showMenu();
       break;
     case '2':
-      showCompleteBatch();
-      showMenu();
       break;
     case '3':
+      showCompleteBatch();
+      showMenu();
       break;
     case '4':
       break;
@@ -189,11 +204,11 @@ void choices() {
 }
 
 int main(int argc, char * argv[]) {
-if (argc == 2) {
+  if (argc == 2) {
     if (!(strcmp(argv[1], "tray.txt"))) {
       showTray(TRUE);
     }
-}
+  }
   showMenu();
   choices();
 }
